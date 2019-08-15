e154cba5a710753d7cdb86e3bbc5c61c2411cf3b
niklasbae <niklasbae@hotmail.com>
2019-08-15 18:29:27 +0200

    simple api

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
-from flask import flask
-from flask_restplus import Api
+from flask import Flask
+from flask_restplus import Api, Resource, fields
+
 
 app = Flask(__name__)
 api = Api(app)
 
+
+a_language = api.model('language', {'language' : fields.String('The language.')})
+
+
+languages = []
+python = {'language' : 'Python'}
+languages.append(python)
+
+@api.route('/language')
+class Language(Resource):
+    def get(self):
+        return languages
+
+    @api.expect(a_language)
+    def post(self):
+        languages.append(api.payload)
+        return {'result' : 'Language added'}, 201
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
